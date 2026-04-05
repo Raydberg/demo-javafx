54ff14384fc20752b41a2a13d121335516038d5e
Raydberg Gabriel <raydbergg@gmail.com>
2026-04-05 17:34:24 -0500

    update

--- src/main/java/com/example/demo/auth/HelloController.java
+++ src/main/java/com/example/demo/auth/HelloController.java
@@ -6,7 +6,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +18,13 @@ import java.io.IOException;
 public class HelloController {
     @FXML
     private Label nameLabel;
+
+    @FXML
+    private Button logoutButton;
+    @FXML
+    private AnchorPane scenePane;
+    Stage stage;
+
 
     @FXML
     void displayName(String name) {
@@ -27,4 +38,20 @@ public class HelloController {
         stage.setScene(new Scene(root));
         stage.show();
     }
+
+    @FXML
+    void logout(ActionEvent event) {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("¿Desea cerrar sesion ?");
+        alert.setContentText("¿Do you want to safe before exiting?");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            stage = (Stage) scenePane.getScene().getWindow();
+            System.out.println("You successfully logged out");
+            stage.close();
+        }
+    }
+
 }
